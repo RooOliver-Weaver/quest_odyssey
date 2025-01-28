bf01d46cff4a026ca25ce7fa6974bb368e398eea
khushbu vohora <gulnaz328@gmail.com>
2025-01-28 12:08:58 +0000

    create/new/forms

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,2 +1,23 @@
 class CampaignsController < ApplicationController
+skip_before_action :authenticate_user!
+  def new
+    @campaign = Campaign.new
+  end
+
+  def create
+    @campaign = Campaign.new(campaign_params)
+    @campign.user = current_user
+    if @campaign.save!
+      redirect_to campaign_path(@campaign)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def campaign_params
+    params.require(:campaign).permit(:name, :setting, :description, :next_session, :notes, :active, :dm_notes, :user)
+  end
+
 end

4ea19b820700496522a479c7dab8d86f76ecdf55
lucas halfpenny <lucashalfpenny@gmail.com>
2025-01-28 14:26:48 +0000

    Added basic show template for characters

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,7 @@
 class Character < ApplicationRecord
   belongs_to :user
+
+  validates :name, :race, :speciality, :level, presence: true
+  validates :level, numericality: { only_integer: true, default: 0, less_than_or_equal_to: 20}
+  validates :speciality, inclusion: { in: %w(barbarian wizard rogue bard cleric fighter sorcerer), message: "%{value} is not a valid speciality" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_28_111254) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_28_125136) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_28_111254) do
     t.text "biography"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "background"
+    t.string "alignment"
     t.index ["user_id"], name: "index_characters_on_user_id"
   end
 

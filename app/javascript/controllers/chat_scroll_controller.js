4ed2c9b9a7abd8fe4b347792fba18a482a3f226a
Richard Carveth <richardcarveth191@hotmail.com>
2025-01-30 20:47:08 +0000

    Chat scroll fixed, added Cloundinary gem and partial setup

--- app/javascript/controllers/chat_scroll_controller.js
+++ app/javascript/controllers/chat_scroll_controller.js
@@ -7,26 +7,10 @@ export default class extends Controller {
   connect() {
     this.scrollToBottom();
 
-    console.log("Connected to chat-scroll controller");
-
-    const currentUserId = parseInt(document.body.dataset.currentUserId, 10);
-    if (this.userIdValue === currentUserId) {
-      this.element.classList.add('sent');
-      this.element.classList.remove('received');
-    } else {
-      this.element.classList.add('received');
-      this.element.classList.remove('sent');
-    }
-    this.scrollToBottom();
+    document.addEventListener('turbo:before-stream-render', this.scrollToBottom.bind(this));
   }
 
   scrollToBottom() {
     this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight;
-    console.log("Scrolled to bottom");
-  }
-
-  newMessageAppeared() {
-    this.scrollToBottom();
-    console.log("New message appeared");
   }
 }

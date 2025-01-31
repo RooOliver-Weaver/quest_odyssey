c04ac2af56b118862e7301b2d2b1f5755d0e7b09
Richard Carveth <richardcarveth191@hotmail.com>
2025-01-31 12:04:49 +0000

    Chat box moved to a small button that expands
    In dire need of styling

--- app/javascript/controllers/chat_scroll_controller.js
+++ app/javascript/controllers/chat_scroll_controller.js
@@ -8,9 +8,23 @@ export default class extends Controller {
     this.scrollToBottom();
 
     document.addEventListener('turbo:before-stream-render', this.scrollToBottom.bind(this));
+
+    document.addEventListener('turbo:render', this.scrollToBottom.bind(this));
+
+    window.addEventListener("chatbox:opened", this.reconnect.bind(this));
   }
 
   scrollToBottom() {
-    this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight;
+    setTimeout(() => {
+      if (this.messagesTarget) {
+        this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight;
+      }
+    }, 0);
+  }
+
+  reconnect() {
+    this.scrollToBottom();
+    document.addEventListener('turbo:before-stream-render', this.scrollToBottom.bind(this));
+    document.addEventListener('turbo:render', this.scrollToBottom.bind(this));
   }
 }

ae662520380e21116793a2be48bbca4295301e26
lucas halfpenny <lucashalfpenny@gmail.com>
2025-02-04 13:07:18 +0000

    Updating chars index and show with new seed content and added a column to chars updating schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_04_092858) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_04_125145) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -99,6 +99,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_04_092858) do
     t.text "personality"
     t.jsonb "equipment", default: []
     t.jsonb "traits", default: []
+    t.jsonb "attacks", default: []
     t.index ["user_id"], name: "index_characters_on_user_id"
   end
 

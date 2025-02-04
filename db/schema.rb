742f4f9b1d1dd8c4d34018906ad0a9fe5ee153cc
Richard Carveth <richardcarveth191@hotmail.com>
2025-02-04 19:55:19 +0000

    weirdness

--- db/schema.rb
+++ db/schema.rb
@@ -97,7 +97,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_04_125145) do
     t.datetime "updated_at", null: false
     t.string "background"
     t.string "alignment"
-    t.string "portrait"
     t.text "personality"
     t.jsonb "equipment", default: []
     t.jsonb "traits", default: []

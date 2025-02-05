99020350a44d728b401b8d5cdd46d0f1113940c0
Richard Carveth <richardcarveth191@hotmail.com>
2025-02-05 17:46:12 +0000

    Now deletes cloudindary uploads on object destruction

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -10,4 +10,30 @@ class Campaign < ApplicationRecord
   multisearchable against: [:name, :description, :setting]
 
   validates :name, :setting, :description, presence: true
+
+  after_create_commit :store_public_id_in_metadata
+  after_destroy :purge_image
+
+  private
+
+  def store_public_id_in_metadata
+    return unless image.attached?
+
+    image_file = image.download
+
+    response = Cloudinary::Uploader.upload(StringIO.new(image_file))
+
+    image.blob.update(metadata: { public_id: response['public_id'] })
+  end
+
+  def purge_image
+    if image.attached? && image.blob.metadata[:public_id]
+      public_id = image.blob.metadata[:public_id]
+      Cloudinary::Uploader.destroy(public_id)
+    else
+      Rails.logger.error "No public_id found for the image attachment."
+    end
+  rescue => e
+    Rails.logger.error "Cloudinary deletion failed for campaign #{id}: #{e.message}"
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -13,4 +13,30 @@ class Character < ApplicationRecord
   validates :level, numericality: { only_integer: true, default: 0, less_than_or_equal_to: 20}
   validates :speciality, inclusion: { in: %w(Barbarian Wizard Rogue Bard Cleric Fighter Sorcerer Druid Ranger Paladin Monk Warlock), message: "%{value} is not a valid speciality" }
   validates :portrait, content_type: { in: [:jpeg, :png, :webp, :tiff, :svg], spoofing_protection: true }, size: { less_than: 5.megabytes, message: "is too big" }
+
+  after_create_commit :store_public_id_in_metadata
+  after_destroy :purge_portrait
+
+  private
+
+  def store_public_id_in_metadata
+    return unless portrait.attached?
+
+    portrait_file = portrait.download
+
+    response = Cloudinary::Uploader.upload(StringIO.new(portrait_file))
+
+    portrait.blob.update(metadata: { public_id: response['public_id'] })
+  end
+
+  def purge_portrait
+    if portrait.attached? && portrait.blob.metadata[:public_id]
+      public_id = portrait.blob.metadata[:public_id]
+      Cloudinary::Uploader.destroy(public_id)
+    else
+      Rails.logger.error "No public_id found for the portrait attachment."
+    end
+  rescue => e
+    Rails.logger.error "Cloudinary deletion failed for campaign #{id}: #{e.message}"
+  end
 end

4a3ebba272bb9dc1d7b0d2ad36ca249a0450d422
Roshan Abeysekera <roshanabey2@gmail.com>
2025-02-05 10:55:48 +0000

    Fixed some typos and disabled create session button if sessions are pending

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_04_183351) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_05_101454) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -102,6 +102,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_04_183351) do
     t.jsonb "equipment", default: []
     t.jsonb "traits", default: []
     t.jsonb "attacks", default: []
+    t.string "portrait"
     t.index ["user_id"], name: "index_characters_on_user_id"
   end
 

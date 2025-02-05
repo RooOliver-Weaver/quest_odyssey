7b7ac41ab08a238b834ca4d6468eee752c48ba45
Richard Carveth <richardcarveth191@hotmail.com>
2025-02-05 15:52:10 +0000

    A few small fixes

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -5,7 +5,7 @@ class Campaign < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :sessions, dependent: :destroy
   has_many :character_sessions, through: :sessions, dependent: :destroy
-  has_one_attached :image
+  has_one_attached :image, dependent: :purge_later
   include PgSearch::Model
   multisearchable against: [:name, :description, :setting]
 

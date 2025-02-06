b23e08033515a9523937d8a8ebf203516ad5f269
rowan oliver weaver <159619990+RooOliver-Weaver@users.noreply.github.com>
2025-02-06 12:39:29 +0000

    dashboard and calendar done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -46,6 +46,10 @@ class PagesController < ApplicationController
 
     # start_date = params.fetch(:start_date, Date.today).to_date
     # @sessions = Session.where(date: start_date.beginning_of_month..start_date.end_of_month)
-    @sessions = Session.where(campaign_id: @campaigns.select { |campaign| campaign.user == current_user }) || Session.where(campaign_id: @campaigns.select { |campaign| campaign.character.user == current_user})
+    dm_sessions = Session.where(campaign_id: @campaigns.map(&:id))
+    player_sessions = Session.joins(:character_sessions)
+                        .where(character_sessions: { campaign_character_id: current_user.campaign_characters.select(:id) })
+    @sessions = dm_sessions + player_sessions
+    @sessions.uniq!
   end
 end

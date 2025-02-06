6ae257466bf6179bc7a95a03112336ffdae16f86
rowan oliver weaver <159619990+RooOliver-Weaver@users.noreply.github.com>
2025-02-06 10:13:37 +0000

    calendar edits

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,13 @@ class CalendarsController < ApplicationController
   def index
     @user = current_user
     @campaigns = @user.campaigns
-    start_date = params.fetch(:start_date, Date.today).to_date
-    @sessions = Session.where(date: start_date.beginning_of_month..start_date.end_of_month)
+    # start_date = params.fetch(:start_date, Date.today).to_date
+    # @sessions = Session.where(date: start_date.beginning_of_month..start_date.end_of_month)
+    @sessions = Session.where(campaign_id: @campaigns.select { |campaign| campaign.user == current_user })
+                || Session.where(campaign_id: @campaigns.select { |campaign| campaign.character.user == current_user})
+
+    # sessions.character_sessions.each do |character_session|
+    #   character.character.user == current_user
+    # end
   end
 end
